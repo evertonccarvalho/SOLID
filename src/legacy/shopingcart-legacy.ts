267bb864898ff6ff35be505f6deeb950a63cc521
Éverton Carvalho <evertonsnake@gmail.com>
2024-01-30 08:32:40 -0300

    Singleton aplicado

--- src/legacy/shopingcart-legacy.ts
+++ src/legacy/shopingcart-legacy.ts
@@ -1,16 +1,18 @@
 type CartItem = { name: string; price: number };
 type OrderStatus = 'open' | 'closed';
 
-export class ShoppingCart {
+export class ShoppingCartLegacy {
   private readonly _items: CartItem[] = [];
   private _orderStatus: OrderStatus = 'open';
 
   addItem(item: CartItem): void {
     this._items.push(item);
   }
+
   removeItem(index: number): void {
     this._items.splice(index, 1);
   }
+
   get items(): Readonly<CartItem>[] {
     return this._items;
   }
@@ -39,6 +41,7 @@ export class ShoppingCart {
   isEmpty(): boolean {
     return this._items.length === 0;
   }
+
   sendMessage(message: string): void {
     console.log('Mensagem enviada', message);
   }
@@ -52,7 +55,7 @@ export class ShoppingCart {
   }
 }
 
-const shoppingCart = new ShoppingCart();
+const shoppingCart = new ShoppingCartLegacy();
 shoppingCart.addItem({ name: 'Camiseta', price: 49.9 });
 shoppingCart.addItem({ name: 'Caderno', price: 9.9 });
 shoppingCart.addItem({ name: 'Lapis', price: 1.59 });
